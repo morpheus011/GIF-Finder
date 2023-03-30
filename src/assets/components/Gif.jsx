6ec1aa39ba9a924f792cbf1f1b9d3e84b51be5ac
Sujit Kumar <mnamesujit@gmail.com>
2023-03-30 14:33:11 +0530

    Search Functionality Complited

--- src/assets/components/Gif.jsx
+++ src/assets/components/Gif.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 const Gif = ({ data }) => {
-    const copyURL = async (link) => {
-     try {
-       await navigator.clipboard.writeText(link);
-       console.log("Content copied to clipboard");
-     } catch (err) {
-       console.error("Failed to copy: ", err);
-     }
-   }; 
+  const copyURL = async (link) => {
+    try {
+      await navigator.clipboard.writeText(link);
+      console.log("Content copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
 
   return (
-      <>
-        {data.map((currGif) => {
-            const { type, id, images } = currGif;
-            return (<div className='gif-cards'>
-              <img src={images.original.url} alt="gif-not-found" />
-                <button className='copy-btn' onClick={(e) => { copyURL(images.original.url)}} > Copy Link </button>
-                </div>
-            )
-        })}
+    <>
+      {data.map((currGif) => {
+        const { type, id, images } = currGif;
+        return (
+          <div className="gif-cards">
+            <img src={images.original.url} alt="gif-not-found" />
+            <button
+              className="copy-btn"
+              onClick={(e) => {
+                copyURL(images.original.url);
+              }}
+            >
+              {" "}
+              Copy Link{" "}
+            </button>
+          </div>
+        );
+      })}
     </>
   );
-}
+};
 
-export default Gif
+export default Gif;
--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,56 +1,51 @@
 import React from "react";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import Gif from "./Gif";
-import API_KEY from '../env/api-key'
- 
-const Home = () => {
+import API_KEY from "../env/api-key";
 
-  const [gifs, setGifs] = useState([])
+const Home = () => {
+  const [gifs, setGifs] = useState([]);
   const [query, setQuery] = useState("beautiful");
 
   const handleChange = async (e) => {
     console.log("Handle change");
-    console.log(e.target.value)
+    console.log(e.target.value);
     setQuery(e.target.value);
-    console.log(query)
-  }
+    console.log(query);
+  };
+  const URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=10&offset=0&rating=g&lang=en`;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(query)
-  }
-
-
-  const URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=10&offset=0&rating=g&lang=en`;
+    fetchGIF(URL);
+  };
 
   useEffect(() => {
     fetchGIF(URL);
-  }, [])
-  
-  const fetchGIF = async(url) => {
+  }, []);
+
+  const fetchGIF = async (url) => {
     try {
       const res = await fetch(url);
       const out = await res.json();
       if (out.data.length > 0) {
-          setGifs(out.data);
-
+        setGifs(out.data);
       }
       console.log(out.data);
-      console.log(gifs)
-    } catch(e) {
+      console.log(gifs);
+    } catch (e) {
       console.error(e);
     }
-  }
+  };
 
-  
-  console.log(query)
+  console.log(query);
   return (
     <div className="main">
-        <form action="" className="search-box">
-          <input type="text" name="query"  onChange={handleChange} />
-          <button onClick={handleSubmit}>Search</button>
-        </form>
-  
+      <form action="" className="search-box">
+        <input type="text" name="query" onChange={handleChange} />
+        <button onClick={handleSubmit}>Search</button>
+      </form>
+
       <h1>{query}</h1>
       <div className="gif-container">
         <Gif data={gifs} />
